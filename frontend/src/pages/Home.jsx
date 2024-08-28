603c3d72eb038817674295c477f29f22ebf5aff0
Ankit Jeena <ankitjeena.08@gmail.com>
2024-08-28 14:14:04 +0530

    change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import BestSeller from "../components/BestSeller"
 import Hero from "../components/Hero"
 import LatestCollection from "../components/LatestCollection"
-import NewsLetterBox from "../components/NewsLetterBox"
+import NewsletterBox from "../components/NewsletterBox"
 import OurPolicy from "../components/OurPolicy"
 
 const Home = () => {
@@ -11,7 +11,7 @@ const Home = () => {
       <LatestCollection/>
       <BestSeller/>
       <OurPolicy/>
-      <NewsLetterBox/>
+      <NewsletterBox/>
     </div>
   )
 }
